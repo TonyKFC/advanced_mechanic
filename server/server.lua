a2e50856a78d3f88ab1784ff8d1da8b6ba8deb87
Tony <108546980+TonyKFC@users.noreply.github.com>
2023-04-29 03:01:46 +0800

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,41 +1,50 @@
 ESX = exports["es_extended"]:getSharedObject()  
-RegisterServerEvent('repairkit:remove')
-AddEventHandler('repairkit:remove', function()
-	local Player = source
+
+
+
+
+TriggerEvent('esx_phone:registerNumber', 'mechanic', 'phone', true, true)
+TriggerEvent('esx_society:registerSociety', 'mechanic', 'mechanic', 'society_mechanic', 'society_mechanic', 'society_mechanic', {type = 'private'})
+
+
+ 
+lib.callback.register('repairkit:remove', function(source, item, metadata, target)
+
+    local Player = source
     exports.ox_inventory:RemoveItem(Player, 'repair_kit', 1)
 
 end)
-RegisterServerEvent('washing:remove')
-AddEventHandler('washing:remove', function()
+
+ 
+
+lib.callback.register('washing:remove', function(source, item, metadata, target)
+    
 	local Player = source
     exports.ox_inventory:RemoveItem(Player, 'wash_tool', 1)
     exports.ox_inventory:RemoveItem(Player, 'sponge', 1) 
 
 end)
 
-RegisterServerEvent('washing:dirtysponge')
-AddEventHandler('washing:dirtysponge', function()
+ 
+lib.callback.register('washing:dirtysponge', function(source, item, metadata, target)
+
 	local Player = source 
     local success = exports.ox_inventory:AddItem(Player, 'dirty_sponge', 1)
 
 end)
 
-RegisterServerEvent('washing:spongeadd')
-AddEventHandler('washing:spongeadd', function()
+lib.callback.register('washing:spongeadd', function(source, item, metadata, target)
 
 	local Player = source 
-    
     exports.ox_inventory:RemoveItem(Player, 'dirty_sponge', 1) 
     local success = exports.ox_inventory:AddItem(Player, 'sponge', 1)
 
-end)    
-
-
-RegisterServerEvent('hackcar:remove')
-AddEventHandler('hackcar:remove', function()
+end)
+ 
+lib.callback.register('hackcar:remove', function(source, item, metadata, target)
 
 	local Player = source 
-    
     exports.ox_inventory:RemoveItem(Player, 'welding_torch', 1)  
 
-end)    
+end) 
+ 
